b805a40fc7de6fac4b0fc4d0dc9cb5dd1abbdfe7
Belkacz <belka.lukasz.job@gmail.com>
2022-01-20 22:31:23 +0100

    działace loswanie i wyniki

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import SlotMachine from "./Slot-machine"
-
+import logo from "./logo.svg";
+import "./App.css";
+import SlotMachine from "./Slot-machine";
 
 function App() {
   return (
     <div className="App">
       {/* <header style={{height: "200px"}} className="App-header">
       </header> */}
-      
+
       <p>Cassino</p>
-  
+
       <SlotMachine />
     </div>
   );
--- src/Slot-machine.js
+++ src/Slot-machine.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
-import './Slot-machine.css'
- 
+import "./Slot-machine.css";
+
 export default function SlotMachine() {
   const [score, setNewScore] = useState([]);
   const [originallist, setOriginalList] = useState([]);
-  const [list, setList] = useState([]);
   const [showresult, setShowResult] = useState(false);
- 
- 
+  const [win, setWin] = useState(0);
+
   useEffect(() => {
     const getList = async () => {
       const resp = await fetch(
@@ -17,86 +16,159 @@ export default function SlotMachine() {
       setOriginalList(data);
     };
     getList();
- 
   }, []);
- 
-  // useEffect(() => {
-  //   fetch("https://my-json-server.typicode.com/Belkacz/cassino-server/roller")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-  // }, []);
- 
-  let counter = 0
- 
-  function roll(counter) {
-    console.log(counter)
-    var intervalID = setInterval(function() {
+
+  function roll() {
+    let counter = 0;
+    let listtocheck = [];
+    if (score.length > 4) {
+      setNewScore([]);
+    }
+    let intervalID = setInterval(function () {
+      setShowResult(true);
       let randomlist = originallist;
       randomlist.sort(() => Math.random() - Math.random());
       let sliceelem = randomlist.slice(0, 1);
-      console.log(sliceelem)
-        setNewScore(prev => [...prev, sliceelem])
-      counter ++
+      listtocheck.push(sliceelem[0]);
+      setNewScore((prev) => [...prev, sliceelem[0]]);
+      counter++;
+      // checkwin(listtocheck);
       if (counter > 4) {
+        checkwin(listtocheck);
         clearInterval(intervalID);
       }
     }, 1000);
- 
- 
-        // setShowResult(false)
-    // let randomlist = originallist;
-    // randomlist.sort(() => Math.random() - Math.random());
-    // let slicelist = randomlist.slice(0, 5);
-    // for (let i= 0; i <4; i++){
-    //   console.log(slicelist)
+  }
+
+  function checkwin(listtocheck) {
+    let bigeststrike = 0;
+
+    listtocheck.forEach((element1) => {
+      let actualstrike = 0;
+      listtocheck.forEach((element2) => {
+        if (element1 === element2) {
+          actualstrike++;
+          if (actualstrike > bigeststrike) {
+            bigeststrike = actualstrike;
+          }
+        }
+      });
+    });
+    console.log(bigeststrike);
+    console.log(bigeststrike > 2);
+    if (bigeststrike > 2) {
+      console.log(bigeststrike > 2);
+      setWin(3);
+    } else if (bigeststrike > 3) {
+      setWin(4);
+    } else if (bigeststrike > 4) {
+      setWin(5);
+    } else {
+      setWin(1);
+    }
+
+    // function compareFrequency(a, b) {
+    //   return frequency[b] - frequency[a];
     // }
-    // setList(slicelist)
-    // setTimeout(() => {
-    //   setShowResult(true);
-    // }, 2137);
-    // let randomElement = list[Math.floor(Math.random()*list.length)]
- 
+
+    // const occurrencesOf = (number, numbers) =>
+    //   numbers.reduce(
+
+    //     (counter, currentNumber) =>
+    //       number === currentNumber ? counter + 1 : counter,
+    //     0
+    //   );
+
+    //   let sortscore = listtocheck.sort(function(a, b) {
+    //     if (a.id === b.id) {
+    //         if(a.order > b.order) return -1; else return 1;
+    //     }
+    // });
+    // console.log(sortscore)
+    // sortscore.forEach((element) => {
+    //   console.log(occurrencesOf(element, sortscore))
+    //   console.log(occurrencesOf(element, sortscore)>2)
+    //   if (occurrencesOf(element, sortscore) > 2) {
+    //     setWin(3);
+    //   } else if (occurrencesOf(element, sortscore) > 3) {
+    //     setWin(3);
+    //   } else if (occurrencesOf(element, sortscore) > 4) {
+    //     setWin(4);
+    //   } else {
+    //     setWin(1);
+    //   }
+    // });
   }
- 
-  const Results = () => (
-    <div id="results">
-      <p >Wylosowano </p>
-      <br></br>
-      <div className="slot">
-      <section>
-      <div className="container">
-      {list.map(({ name, id }) => {
-        return (
-          <div key={id }>
-          <span>{name}</span>
-          </div>
-        );
-      })}
+
+  // setShowResult(false)
+  // let randomlist = originallist;
+  // randomlist.sort(() => Math.random() - Math.random());
+  // let slicelist = randomlist.slice(0, 5);
+  // for (let i= 0; i <4; i++){
+  //   console.log(slicelist)
+  // }
+  // setList(slicelist)
+  // setTimeout(() => {
+  //   setShowResult(true);
+  // }, 2137);
+  // let randomElement = list[Math.floor(Math.random()*list.length)]
+
+  const Results = () => {
+    let winorlose = <div></div>;
+    if (score.length === 5) {
+      if (win === 1) {
+        winorlose = <p>przegrałes</p>;
+      } else if (win === 0) {
+        winorlose = null;
+      } else if (win === 3) {
+        winorlose = <p>trafiłeś {win}</p>;
+      } else if (win === 4) {
+        winorlose = <p>trafiłeś {win}</p>;
+      } else if (win === 5) {
+        winorlose = <p>trafiłeś {win}</p>;
+      }
+    }
+    // switch (win) {
+    //   case 1:
+    //     winorlose = <p>przegrałeś</p>;
+    //     break;
+    //   case 3:
+    //     winorlose = <p>trafiłeś 3</p>;
+    //     break;
+    //   case 4:
+    //     winorlose = <p>trafiłeś 4</p>;
+    //     break;
+    //   case 5:
+    //     winorlose = <p>trafiłeś 5</p>;
+    //     break;
+    // }
+    return (
+      <div id="results">
+        <p>Wylosowano </p>
+        <br></br>
+        <div className="slot">
+          <section>
+            <div className="container">
+              {score.map(({ name, id }, index) => {
+                return (
+                  <div key={id + name + index}>
+                    <span>{name}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </section>
+        </div>
+        <div>{winorlose}</div>
       </div>
-      </section>
-      </div>
-    </div>
-  );
- 
-  function test(e) {
-    console.log(e)
-    console.log("start")
- 
-  }
- 
- 
+    );
+  };
+
   return (
     <div>
+      <button onClick={roll}>losuj</button>
+      <br></br>
       {showresult ? <Results /> : null}
-      <button
-        onClick={roll}
-      >
-        losuj
-      </button>
       <br></br>
     </div>
   );
